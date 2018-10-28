6f95f58ec6279fcb022b1da517f9ef665e87c684
superopenai <37723411+superopenai@users.noreply.github.com>
2018-10-28 10:25:58 +0800

    爬聊大优秀教师

--- little_spider/spiders/kevin.py
+++ little_spider/spiders/kevin.py
@@ -18,14 +18,29 @@ class KevinSpider(scrapy.Spider):
     #
     #     pprint(imf)
     #     pass
-
+    print("爬取聊大师资团队")
     def parse(self, response):
-
+        print("聊大优秀教师")
         post=response.xpath("//*[@id=\"height2\"]/div[2]/ul/li[2]/div[2]/ol/li/a/text()").extract()
+        print(post)
+        print("享受国务院特殊津贴的专家")
+        postget1 = response.xpath ( "//*[@id=\"height2\"]/div[2]/ul/li[2]/div[2]/ol/li[2]/a/text()" ).extract ()
+        print(postget1)
+        print ( "泰山学者" )
+        postget2 = response.xpath ( "//*[@id=\"height2\"]/div[2]/ul/li[2]/div[2]/ol/li[3]/a/text()" ).extract ()
+        print ( postget2 )
+        print ( "山东省有突出贡献的中青年专家" )
+        postget3 = response.xpath ( "///*[@id=\"height2\"]/div[2]/ul/li[2]/div[3]/ol/li[1]/a/text()" ).extract ()
+        print ( postget3)
+        print ( "山东省优秀教师" )
+        postget4 = response.xpath ( "//*[@id=\"height2\"]/div[2]/ul/li[2]/div[3]/ol/li[2]/a/text()" ).extract ()
+        print ( postget4 )
+        print("省级教学名师")
+        postget5 = response.xpath ( "//*[@id=\"height2\"]/div[2]/ul/li[2]/div[3]/ol/li[3]/a/text()" ).extract ()
+        print(postget5)
 
 
-
-# 并不是直接print(res)来调试的,我在主目录建立一个mian入口函数
+# 并不是直接print(res)来调试   的,我在主目录建立一个mian入口函数
 # 使用scrapy.cmdline的执行函数
 
 
